1eef033a53811155ccacb41d54fabd5ca669d788
DuskyElf <clayedison75@gmail.com>
2022-09-20 18:24:55 +0530

    Explaination

--- src/nth_prime.rs
+++ src/nth_prime.rs
@@ -1,7 +1,9 @@
 // Problem id 7
 // https://projecteuler.net/problem=7
 
+// This finds the nth prime number
 pub fn solution(n: u64) -> u64 {
+    // First prime in 2
     if n == 1 {
         return 2;
     }
@@ -10,6 +12,7 @@ pub fn solution(n: u64) -> u64 {
     let mut prime = 2;
     let mut trial = 3;
 
+    // Trialing each number to be prime untill finding the nth prime
     let mut i = 0;
     while i != n {
         if new_is_prime(trial, &mut primes) {
@@ -24,6 +27,8 @@ pub fn solution(n: u64) -> u64 {
 }
 
 fn new_is_prime(n: u64, primes: &mut Vec<u64>) -> bool {
+    // Checking for each previous prime as it's factor
+    // till n / 2, because prime factors of n never gonna be higher than n / 2
     for prime in primes.iter() {
         if *prime >= n / 2 {
             break;
